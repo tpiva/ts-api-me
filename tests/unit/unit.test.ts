e2f7591bb6d482d5f48f4fc508736fbc7cc6c053
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2019-08-11 18:12:39 -0300

    adição de tests

---
+++ tests/unit/unit.test.ts
@@ -0,0 +1,27 @@
+import { testDouble, expect } from './config/helpers';
+
+describe('Unit Test controller', () => {
+    describe('Method create', () => {
+        it('Deve criar um novo usuário', () => {
+
+        });
+    });
+
+    describe('Mehtod update', () => {
+        it('Deve atualizar um usuário', ()=> {
+
+        });
+    });
+
+    describe('Mehtod getUsers', () => {
+        it('Deve retornar uma lista de usuários', ()=> {
+
+        });
+    });
+
+    describe('Mehtod delete', () => {
+        it('Deve deletar um usuário', ()=> {
+
+        });
+    });
+});
