baca9c0fca66c039677f7adbd19e2d1d58a975f6
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2019-08-12 20:56:43 -0300

    refactor do User/router

--- server/modules/User/routes.ts
+++ server/modules/User/routes.ts
@@ -1,27 +1,30 @@
 import { Request, Response } from 'express';
+import UserController from './controller';
 
+let UserCtrl;
 class UserRoutes{
 
     constructor() {
-        
+        UserCtrl = new UserController();        
     }
 
     index(req: Request, res: Response) {
-
+        return UserCtrl.getAll(req, res);
     }
 
     create(req: Request, res: Response) {
-        
+        return UserCtrl.createUser(req, res);
     }
 
     findOne(req: Request, res: Response) {
-        
+        return UserCtrl.getById(req, res);        
     }
     update(req: Request, res: Response) {
-        
+        return UserCtrl.updateUser(req, res);
     }
+
     delete(req: Request, res: Response) {
-        
+        return UserCtrl.deleteUser(req, res);
     }
 }
 
