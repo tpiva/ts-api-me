e02acab635b48d05005598a3c0fd78d957560ba7
Thiago Piva Magalaes <thiago.p.magalhaes@gmail.com>
2019-08-12 15:36:14 -0300

    adicao do userRouter

--- server/api/routes/routes.ts
+++ server/api/routes/routes.ts
@@ -1,15 +1,21 @@
 import { Application, Request, Response } from 'express';
-import { request } from 'https';
+import UserRoutes from '../../modules/User/routes';
 
 class Routes {
 
+    private router: UserRoutes;
+
     constructor(app: Application) {
+        this.router = new UserRoutes();
         this.getRoutes(app);        
     }
 
     getRoutes(app: Application): void {
-        app.route('/').get((_req: Request, res: Response) => res.send('hello'));
-        app.route('/ola/:nome').get((req: Request, res: Response) => res.send(`Hello, ${req.params.nome}`));
+        app.route('/api/users/all').get(this.router.index);
+        app.route('/api/users/create').get(this.router.create);
+        app.route('/api/users/:id').get(this.router.findOne);
+        app.route('/api/users/:id/update').get(this.router.update);
+        app.route('/api/users/:id/destroy').get(this.router.delete);
     }
 }
 
---
+++ server/modules/User/routes.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+
+class UserRoutes{
+
+    constructor() {
+        
+    }
+
+    index(req: Request, res: Response) {
+
+    }
+
+    create(req: Request, res: Response) {
+        
+    }
+
+    findOne(req: Request, res: Response) {
+        
+    }
+    update(req: Request, res: Response) {
+        
+    }
+    delete(req: Request, res: Response) {
+        
+    }
+}
+
+export default UserRoutes;
