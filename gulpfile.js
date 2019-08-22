cd7b8ffaf22d307f4f52b8d8afa6a5a6c8644173
Thiago Piva Magalaes <thiago.p.magalhaes@gmail.com>
2019-08-22 15:52:36 -0300

    aula 112 task clean gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,11 @@ const ts = require('gulp-typescript');
 
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('compile', () => {
+gulp.task('clean', () => {
+    return gulp.src('dist').pipe(clean());
+});
+
+gulp.task('compile', gulp.series(['clean']), () => {
     return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('dist'));
 });
 
