cacac75c6c8f0d927519f357a219440b30ec3d2e
Thiago Piva Magalaes <thiago.p.magalhaes@gmail.com>
2019-08-26 16:13:38 -0300

    aula 122 criando modulo do author

--- server/api/routes/routes.ts
+++ server/api/routes/routes.ts
@@ -1,6 +1,7 @@
 import { Application } from 'express';
 import UserRoutes from '../../modules/User/routes';
 import TokenRoutes from '../../modules/auth/auth';
+import AuthorRoutes from '../../modules/Author/routes';
 
 class Routes {
 
@@ -12,6 +13,12 @@ class Routes {
         app.route('/api/users/:id').all(auth.config().authenticate()).get(UserRoutes.findOne);
         app.route('/api/users/:id/update').all(auth.config().authenticate()).put(UserRoutes.update);
         app.route('/api/users/:id/destroy').all(auth.config().authenticate()).delete(UserRoutes.delete);
+
+        app.route('/api/author/all').get(AuthorRoutes.index);
+        app.route('/api/author/create').post(AuthorRoutes.create);
+        app.route('/api/author/:id').get(AuthorRoutes.findOne);
+        app.route('/api/author/:id/update').put(AuthorRoutes.update);
+        app.route('/api/author/:id/destroy').delete(AuthorRoutes.destroy);
         app.route('/token').post(TokenRoutes.auth);
     }
 }
---
+++ server/modules/Author/service.ts
@@ -0,0 +1,45 @@
+import { IAuthor, createAuthor, createAuthors } from './interface';
+import * as BlueBird from 'bluebird';
+const model = require('../../models');
+
+class Author implements IAuthor {
+    public id: number;
+    public name: string;
+
+    create(author: any) {
+        return model.Author.create(author);
+    }
+
+    getAll(): BlueBird<IAuthor> {
+        return model.Author.findAll({
+            order: ['name']
+        })
+        .then(createAuthors);
+    }
+
+    getById(id: number): BlueBird<IAuthor> {
+        return model.Author.findOne({
+            where: { id },
+            order: ['name']
+        })
+        .then(createAuthor);
+    }
+
+    update(id: number, author: any) {
+        return model.Author.update(author, {
+            where: { id },
+            fields: ['name'],
+            hooks: true,
+            indiviadualHooks: true
+        })
+        .then(createAuthor);
+    }
+
+    delete(id: number) {
+        return model.Author.destroy({
+            where: { id }
+        });
+    }
+}
+
+export default new Author();
