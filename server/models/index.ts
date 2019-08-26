9850c31a978d9ca7308691b60a955ce3cb23f8f3
Thiago Piva Magalaes <thiago.p.magalhaes@gmail.com>
2019-08-26 14:07:36 -0300

    aula 120 definicao de modelos - correcao do index

--- server/models/index.ts
+++ server/models/index.ts
@@ -19,7 +19,11 @@ if (config.dbURL) {
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    let extension = '.js'
+    if (process.env.NODE_ENV == 'development') {
+      extension = '.ts'
+    }
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === `${extension}`);
   })
   .forEach(file => {
     const model = sequelize['import'](path.join(__dirname, file));
