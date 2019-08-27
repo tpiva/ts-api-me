6851915d8db5ab934445de79fc752de27794659b
Thiago Piva Magalaes <thiago.p.magalhaes@gmail.com>
2019-08-27 15:43:24 -0300

    aula 134 implementacao do routermodule segunda parte

---
+++ src/core/router/router-map.ts
@@ -0,0 +1,27 @@
+import { ModuleEndpointMap } from "./base-router-module";
+
+export class RouterModuleFactory {
+    
+    private routerModulesMap: Array<ModuleEndpointMap> = [];
+
+    constructor() {
+        this.bootstrapModules(new ModulesRouterRapper());
+    }
+
+    private bootstrapModules(routerModulesRapper: ModulesRouterRapper) {
+        this.routerModulesMap = routerModulesRapper
+        .registeredModules.map(this.createModules.bind(this));
+    }
+
+    private createModules(registeredModule: FeatureModuleRouter): Array<ModuleEndpointMap> {
+        const { moduleName, parser } = registeredModule;
+        return new moduleName()[parser]();
+    }
+
+    public getRegisteredModules() {
+        return this.routerModulesMap.map((routerModule: ModuleEndpointMap) => {
+            const moduleName: string = Object.keys(routerModule)[0];
+            return routerModule[moduleName];
+        })
+    }
+}
