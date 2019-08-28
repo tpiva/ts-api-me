65117a3064d8cfea3d1c348dbbbe7b0205f40c23
Thiago Piva Magalaes <thiago.p.magalhaes@gmail.com>
2019-08-28 08:39:01 -0300

    aula 137 termino do route module

---
+++ src/core/router/routes.ts
@@ -0,0 +1,40 @@
+import { Application } from 'express';
+import { RouterModuleFactory } from './router-map';
+import { HttpVerbMap, FeatureModuleRouterInfo } from './base-router-module';
+
+export class RouterModule {
+    private routerFactory: RouterModuleFactory;
+    private express: Application;
+
+    constructor(app: Application) {
+        this.express = app;
+        this.routerFactory = new RouterModuleFactory();
+    }
+
+    public exposeRoutes(authenticate?: Function): void {
+        const registerModules = this.routerFactory.getRegisteredModules();
+        if (registerModules && Array.isArray(registerModules)) {
+            registerModules.forEach(this.extractRouterInfoFromModule.bind(this, authenticate));
+        }
+    }
+
+    private extractRouterInfoFromModule(authenticate: Function, routerFeatModule: HttpVerbMap) {
+        if (routerFeatModule) {
+            const registeredVerbs = Object.keys(routerFeatModule);
+            registeredVerbs.forEach(this.extractInfoByVerb.bind(this, authenticate, routerFeatModule));
+        }
+    }
+
+    private extractInfoByVerb(authenticate: Function, routerFeatModule: HttpVerbMap, registeredVerb: string) {
+        routerFeatModule[registeredVerb].forEach(this.mountRoutes.bind(this, authenticate, registeredVerb));
+    }
+
+    private mountRoutes(authenticate: Function, registeredVerb: string, routerInfo: FeatureModuleRouterInfo) {
+        if (routerInfo) {
+            const { isProtected, callback, endpoint } = routerInfo;
+            isProtected
+                ? this.express.route(endpoint).all(authenticate())[registeredVerb](callback)
+                : this.express.route(endpoint)[registeredVerb](callback);
+        }
+    }
+}
